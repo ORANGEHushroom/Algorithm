c1ea4262a88878367e4c035a2948dca59a0503ef
ORANGEHushroom <84065462+ORANGEHushroom@users.noreply.github.com>
2021-08-12 22:58:06 +0900

    0812

---
+++ IM/BOJ_2628.py
@@ -0,0 +1,51 @@
+width, length = map(int,input().split())
+
+num_lines = int(input())
+
+
+for _ in range(len(num_lines)):
+    x, y = map(int,input().split())
+    for i in range(width):
+        for j in range(length):
+            if x == 0:
+                if int(length//2) < y:
+                    result = y * width
+                else:
+                    result = (length-y) * width
+            elif x == 1:
+                if int(width//2) < x:
+                    result = x * length
+                else:
+                    result = (width-x) * length
+
+
+
+# a , y1 = map(int,input().split()) #가로로 슬라이스 0 3
+# b, x1 = map(int,input().split()) # 세로 슬라이스 1 4 
+# c, y2 = map(int,input().split()) # 0 2
+
+
+
+#x, y = map(int, input().split())
+#garo = [0, x]  # 가로
+#sero = [0, y]  # 세로
+#cut_times = int(input())
+#for i in range(cut_times):
+#    num, line_num = map(int, input().split())
+#    if num == 0:  
+#    # num이 0이면 문제에서 가로로 '자르는' 점선이라 했으므로, 이 선은 엄연히 세로에 속하는 선이다.
+#        sero.append(line_num)
+#    else:  
+#    # 반대의 경우도 마찬가지다. 속지 말자.
+#        garo.append(line_num)
+
+#garo = sorted(garo)
+#sero = sorted(sero)
+#result_list = []
+#for i in range(1, len(garo)):
+#    for j in range(1, len(sero)):
+#        width = garo[i] - garo[i-1]
+#        height = sero[j] - sero[j-1]
+#        result_list.append(width * height)
+
+#print(max(result_list))
--- IM/BOJ_2669.py
+++ IM/BOJ_2669.py
@@ -1,10 +1,14 @@
-arr = [[0] * 100 for _ in range(100)]
-cnt = 0
+arr = [[0] * 100 for _ in range(100)] # 빈 박스 만들기
+count = 0
 for i in range(4):
-    x_l, y_l, x_r, y_r = map(int, input().split())
-    for j in range(x_l, x_r):
-        for k in range(y_l, y_r):
-            if arr[k][j] == 0:
-                arr[k][j] = 1
-                cnt += 1
-print(cnt)
+    
+    xl, yl, x2, y2 = map(int, input().split())
+    for j in range(xl, x2):  #박스의 밑
+        for k in range(yl, y2): #박스의 높이
+            
+            if arr[k][j] == 0:  # 좌표가 비어있을때  
+                arr[k][j] = 1 # 1넣어주기
+                count += 1   #면적 하나 추가해주기
+            else:
+                continue   #이미 좌표가 차있다면 패스
+print(count)
