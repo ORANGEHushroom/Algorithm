9791dbd684e0ecdce863384d389297c8e9346f26
ORANGEHushroom <84065462+ORANGEHushroom@users.noreply.github.com>
2021-09-15 15:55:04 +0900

    0915

--- SWEA/D2_2/1927.py
+++ SWEA/D2_2/1927.py
@@ -5,11 +5,15 @@ n = int(input())
 a = ['3','6','9']
 for i in range(1, n+1):
     b = str(i)
-    cnt = 0
-    for j in b:
-        if j in a:
-            cnt+=1
-    print('{}'.format('-'*cnt),end=" ")
-        
+
     if '3' not in b and '6' not in b and '9' not in b:
         print(b,end=" ")
+    
+    else:
+        cnt = 0
+        for j in b:
+            if j in a:
+                cnt+=1
+        print('{}'.format('-'*cnt),end=" ")
+        
+    
