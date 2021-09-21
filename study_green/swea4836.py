9d62e350f53d9da333d3d99a2ef78274113636de
ORANGEHushroom <84065462+ORANGEHushroom@users.noreply.github.com>
2021-09-21 22:39:10 +0900

    0921

--- study_green/swea4836.py
+++ study_green/swea4836.py
@@ -6,6 +6,7 @@ for tc in range(1, T+1):
     N = int(input())
     cnt = 0
     box = [[0]*10 for _ in range(10)]
+    
     for i in range(N):
         x1, y1, x2, y2, color = map(int, input().split()) 
         for x in range(x1,x2+1):
